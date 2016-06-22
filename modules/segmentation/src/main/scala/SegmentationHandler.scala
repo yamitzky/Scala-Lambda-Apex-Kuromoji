09ce49980fb26a3b427660f15eba68ce51c09a58
Yamitzky <negiga@gmail.com>
2016-06-23 02:12:17 +0900

    Define base handler class to avoid boilerplate

--- modules/segmentation/src/main/scala/SegmentationHandler.scala
+++ modules/segmentation/src/main/scala/SegmentationHandler.scala
@@ -1,36 +1,23 @@
-import java.io.{ByteArrayOutputStream, ByteArrayInputStream, InputStream, OutputStream}
-import SegmentationHandler.{Output, Input}
-import org.json4s._
-import org.json4s.jackson.Serialization
 import net.jxpress.kuromoji4s._
 
-
-class SegmentationHandler {
-
-  implicit val formats = org.json4s.DefaultFormats
-  val tokenizer = Tokenizer()
-
-  /**
-    * Lambda handler for the word segmentation function.
-    * This is a vanilla implementation.
-    *
-    * @param inputStream Stream to read request body. Specification by Lambda
-    * @param outputStream Stream to write response. Specification by Lambda
-    */
-  def handler(inputStream: InputStream, outputStream: OutputStream): Unit = {
-    // InputStream -> SomeInputClass
-    val input = Serialization.read[Input](inputStream)
-
-    val tokens = tokenizer.apply(input.sentence)
-
-    // SomeOutputClass -> OutputStream
-    val outputJson = Serialization.write(Output(tokens))
-    outputStream.write(outputJson.getBytes("UTF-8"))
-  }
-
-}
+class SegmentationHandler extends Handler(SegmentationHandler.segmentationHandler)
 
 object SegmentationHandler {
   case class Input(sentence: String)
   case class Output[T](tokens: Seq[T])
+
+  val tokenizer = Tokenizer()
+
+  /**
+    * Execute word segmentation.
+    *
+    * This functions is passed to companion class's `delegate` parameter.
+    *
+    * @param input Input payload to be deserialized from JSON.
+    * @return Output payload to be serialized to JSON
+    */
+  def segmentationHandler(input: Input): Output[ipadic.Token] = {
+    val tokens = tokenizer.apply(input.sentence)
+    Output(tokens)
+  }
 }
